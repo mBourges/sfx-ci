51ff3e2e316537a0ff4741896c4cb0c558e7765f
Matt B <m.bourges@gmail.com>
2020-04-03 17:22:54 +0200

    style: remove dead code

--- utils/source.js
+++ utils/source.js
@@ -1,14 +1,16 @@
-const CommandStream = require('./command-stream');
+const CommandStream = require('../lib/command-stream');
 
-function push(alias) {
+function push({ alias }) {
   return new Promise((resolve, reject) => {
     const child = new CommandStream(
       'sfdx',
-      ['force:source:push', '--json', '-u', alias,'-w', '60'],
+      ['force:source:push', '--json', '-u', alias, '-w', '60'],
       `Push sources to ${alias}`
     );
 
-    child.on('done', result => { resolve(result); });
+    child.on('done', result => {
+      resolve(result);
+    });
     child.execute();
   });
 }
