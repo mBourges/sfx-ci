8c637b9ffa10e1d7a5386db122bf338b4f139c95
Matt B <m.bourges@gmail.com>
2020-04-12 15:15:26 +0200

    fix: make -v available for cmd & show version when command is not specified

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-const minimist = require('minimist');
+const minimist = require("minimist");
 
 module.exports = async () => {
   const args = minimist(process.argv.slice(2));
-  let cmd = args._.length != 1 ? 'help' : args._[0];
+  let cmd = args._.length != 1 ? "help" : args._[0];
 
-  if (args.version || args.v) {
-    cmd = 'version'
+  if (!args._[0] && (args.version || args.v)) {
+    cmd = "version";
   }
 
   if (args.help || args.h) {
-    cmd = 'help'
+    cmd = "help";
   }
-  try{
+  try {
     switch (cmd) {
-      case 'create':
-        await require('./cmds/create')(args)
-        break
+      case "create":
+        await require("./cmds/create")(args);
+        break;
 
-      case 'push':
-        await require('./cmds/push')(args)
-        break
+      case "push":
+        await require("./cmds/push")(args);
+        break;
 
-      case 'test':
-        await require('./cmds/test')(args)
-        break
+      case "test":
+        await require("./cmds/test")(args);
+        break;
 
-      case 'package':
-        await require('./cmds/package')(args)
-        break
+      case "package":
+        await require("./cmds/package")(args);
+        break;
 
-      case 'install':
-        await require('./cmds/install')(args)
-        break
+      case "install":
+        await require("./cmds/install")(args);
+        break;
 
-      case 'version':
-        require('./cmds/version')()
-        break
+      case "version":
+        require("./cmds/version")();
+        break;
 
-      case 'help':
-        require('./cmds/help')(args)
-        break
+      case "help":
+        require("./cmds/help")(args);
+        break;
 
       default:
-        console.error(`"${cmd}" is not a valid command!`)
-        break
+        console.error(`"${cmd}" is not a valid command!`);
+        break;
     }
   } catch (exception) {
     if (exception.exit) {
       exception.exit();
     } else {
-      console.log(exception)
+      console.log(exception);
     }
   }
-}
+};
